8df092d7a51ccbfaaac32991384ca95e4ea8099f
americo stoppello <americo.stoppello@gmail.com>
2020-10-05 16:15:40 +0200

    initial commit

---
+++ src/main/java/astoppello/springframework/incomebalance/IncomebalanceApplication.java
@@ -0,0 +1,13 @@
+package astoppello.springframework.incomebalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomebalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncomebalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/astoppello/springframework/incomebalance/IncomebalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package astoppello.springframework.incomebalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomebalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
